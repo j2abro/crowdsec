32cf52f16b41b8928eddb67451d03e288730309c
John Abraham <j2abro@gmail.com>
2013-02-22 14:48:57 -0800

    testing templates

--- myapp.py
+++ myapp.py
@@ -21,7 +21,7 @@ listdata = ['one', 'two', 'three', 'four']
 
 @app.route('/idealist/')
 def idealist(listdata):
-	return render_template('idealist.html', idealist=idealist)
+	return render_template('idealist.html', listdata=listdata)
 		
 @app.route('/user/<name>')
 def show_name(name):
