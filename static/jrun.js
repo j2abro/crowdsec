dae61e9ba2b67e94efcd8e5a7d14180a6adc1985
John Abraham <j2abro@gmail.com>
2013-02-23 07:53:36 -0800

    testing templates

--- static/jrun.js
+++ static/jrun.js
@@ -1,14 +1,14 @@
 /*
 setTimeout(function() { alert("OK, later, show this"); }, 200);
 */
-document.write("<h1>external file called, bro....</h1>");
+document.write("external file called, bro....");
 
 var listobject = {
-	one: "hello",
-	two: "world",	
-	three: "bit",
-	four: "star",
-	five: "fish",
-	six: "again"
+	one: "hello111",
+	two: "world222",	
+	three: "bit333",
+	four: "star444",
+	five: "fish555",
+	six: "again666"
 };
 
