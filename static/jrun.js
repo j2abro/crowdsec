a0a6af28aa43a95e2b9e390e1ec88856d9e4429b
John Abraham <j2abro@gmail.com>
2013-02-23 15:39:02 -0800

    testing templates

--- static/jrun.js
+++ static/jrun.js
@@ -5,8 +5,8 @@ document.write("external file called, bro.... yes and we've changed it");
 
 var listobject = {
 	one: "hello1111111",
-	two: "world222",	
-	three: "bit333",
+	two: "world2222",	
+	three: "bit3333",
 	four: "star444",
 	five: "fish555",
 	six: "again666",
