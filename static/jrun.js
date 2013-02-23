37dc21d14568b377cfd1089d659f9a9f22b6255b
John Abraham <j2abro@gmail.com>
2013-02-23 15:50:56 -0800

    testing templates

--- static/jrun.js
+++ static/jrun.js
@@ -1,7 +1,7 @@
 /*
 setTimeout(function() { alert("OK, later, show this"); }, 200);
 */
-document.write("external file called, bro.... yes and we've changed it");
+document.write("external file called, bro.... yes and we've changed it"); 
 
 var listobject2 = {
 	one: "hello1111111",
