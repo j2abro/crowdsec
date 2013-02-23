09fcd4a83e3ec7ff266088c585d979d434cfb5d5
John Abraham <j2abro@gmail.com>
2013-02-22 17:36:37 -0800

    testing templates

--- static/jrun.js
+++ static/jrun.js
@@ -1,3 +1,15 @@
-setTimeout(function() { alert("hello world, script style"); }, 2000);
 
+// setTimeout(function() { alert("hello world, script style"); }, 2000);
+
+//  ============================================================================
+//  idea list
+//  ============================================================================
+var listobject = {
+	one: "hello",
+	two: "world",	
+	three: "bit",
+	four: "star",
+	five: "fish",
+	six: "again"
+}
 
