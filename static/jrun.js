30be7718898a4be442534b3482f81c8096eccd08
John Abraham <j2abro@gmail.com>
2013-02-24 11:05:00 -0800

    testing templates

--- static/jrun.js
+++ static/jrun.js
@@ -13,4 +13,32 @@ var listobject2 = {
 	seven: "nowfinally"
 }; 
 
+var workoutSpeed = [
+	{	name: "Idea List",	
+		desc: "Create an ongoing list of tech and business model concepts. This list. Make it interactive where users could sort and add their own ideas.",	
+		catgory: "Miscelaneous",
+		enterprise: true,
+		passion: 5,
+		recurring_rev: true,
+		tech_risk: 1,
+		scalability: 5,
+		phased: 5,
+		sales_density: 5,
+		big_potential: 2,				
+	},
+	{	name: "McRun",	
+		desc: "Add features to existing app.",	
+		catgory: "Sports",
+		enterprise: false,
+		passion: 3,
+		recurring_rev: true,
+		tech_risk: 2,
+		scalability: 3,
+		phased: 4,
+		sales_density: 5,
+		big_potential: 1,				
+	}
+	
+];
+
 
