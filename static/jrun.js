122beba5b28dcfc9497d0bc5c4287216d194768c
John Abraham <j2abro@gmail.com>
2013-02-23 16:10:46 -0800

    testing templates

--- static/jrun.js
+++ static/jrun.js
@@ -1,15 +1,20 @@
 /*
 setTimeout(function() { alert("OK, later, show this"); }, 200);
 */
-document.write("external file called, bro.... yes and we've changed it"); 
+document.write("external jrun.js loaded...."); 
 
 var listobject2 = {
-	one: "hello1111111",
-	two: "world2222",	
-	three: "bit3333",
-	four: "star444",
-	five: "fish555",
-	six: "again666",
-	seven: "nowfinally777"
+	one: "hello",
+	two: "world",	
+	three: "bit",
+	four: "star",
+	five: "fish",
+	six: "again",
+	seven: "nowfinally"
 }; 
+
+var ilist {
+	
+	
+}
 
